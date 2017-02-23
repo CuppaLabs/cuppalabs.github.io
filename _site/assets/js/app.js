32f7320609feb6347a487cab76d7b99d268badfe
pradeep <solomon.terli@gmail.com>
2017-02-24 03:02:49 +0530

    new website version
    Cuppa labs new look with new design

---
+++ _site/assets/js/app.js
@@ -0,0 +1,16 @@
+var winkelStore = angular.module("winkelStore",[]);
+
+winkelStore.controller("storeController",function($scope){
+	$scope.items = [
+	                {"title":"Angular Circular Menu","author":"Pradeep Kumar Terli","url":"../AngularCircularMenu/index.html","thumbnail":"assets/images/banner.png"},
+	                {"title":"Mac style menu using Angular","author":"Pradeep Kumar Terli","url":"../AngularCircularMenu/index.html","thumbnail":"assets/images/shades-thumbnail2.png"},
+	                {"title":"Angular Circular Menu","author":"Pradeep Kumar Terli","url":"../AngularCircularMenu/index.html","thumbnail":"assets/images/gradient-background-reflection.png"},
+	                {"title":"Angular Circular Menu","author":"Pradeep Kumar Terli","url":"../AngularCircularMenu/index.html","thumbnail":"assets/images/banner.png"},
+	                {"title":"Angular Circular Menu","author":"Pradeep Kumar Terli","url":"../AngularCircularMenu/index.html","thumbnail":"assets/images/shades-thumbnail2.png"},
+	                {"title":"Angular Circular Menu","author":"Pradeep Kumar Terli","url":"../AngularCircularMenu/index.html","thumbnail":"assets/images/gradient-background-reflection.png"},
+	                {"title":"Angular Circular Menu","author":"Pradeep Kumar Terli","url":"../AngularCircularMenu/index.html","thumbnail":"assets/images/banner.png"},
+	                {"title":"Angular Circular Menu","author":"Pradeep Kumar Terli","url":"../AngularCircularMenu/index.html","thumbnail":"assets/images/shades-thumbnail2.png"},
+	                {"title":"Angular Circular Menu","author":"Pradeep Kumar Terli","url":"../AngularCircularMenu/index.html","thumbnail":"assets/images/gradient-background-reflection.png"},
+	                {"title":"Angular Circular Menu","author":"Pradeep Kumar Terli","url":"../AngularCircularMenu/index.html","thumbnail":"assets/images/gradient-background-reflection.png"}
+	                ]
+});
